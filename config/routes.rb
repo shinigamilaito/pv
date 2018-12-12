2ab301a7349eab52a2050b0df2ec83c7e221384b
Edgar <edgareduardoespindola@gmail.com>
2018-12-12 07:11:57 -0600

    Agregando formulario busqueda de equipo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :clients
   resources :employes
 
-  resources :sale_supports
+  resources :sale_supports do
+    get 'search', on: :collection
+  end
   resources :equipment_customers
 
   devise_for :users
