4f3aa32b203887e15749b65f8c64f8ee6f5e07a7
edgar_espindola <edgar.espindola@societao.com>
2018-12-12 16:59:25 -0600

    Se agrega modelos para tipos de pagos y tipos de clientes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181212023333) do
+ActiveRecord::Schema.define(version: 20181212224440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "client_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -44,6 +50,12 @@ ActiveRecord::Schema.define(version: 20181212023333) do
   end
 
   create_table "equipments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payment_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
