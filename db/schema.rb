cf92bdb90173c7e5e56446185f002313b058e46b
edgar_espindola <edgar.espindola@societao.com>
2018-12-12 15:47:15 -0600

    Se agrega el archivo de la base de datos.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181212023333) do
+ActiveRecord::Schema.define(version: 20181212193327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 20181212023333) do
     t.string "name"
   end
 
+  create_table "sale_supports", force: :cascade do |t|
+    t.bigint "equipment_customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["equipment_customer_id"], name: "index_sale_supports_on_equipment_customer_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "first_name", default: "", null: false
@@ -74,4 +81,5 @@ ActiveRecord::Schema.define(version: 20181212023333) do
   add_foreign_key "equipment_customers", "brands"
   add_foreign_key "equipment_customers", "clients"
   add_foreign_key "equipment_customers", "equipments"
+  add_foreign_key "sale_supports", "equipment_customers"
 end
