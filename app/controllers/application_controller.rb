36dd1d694c3596925fc51ced1781a5decc020ad1
edgar_espindola <edgar.espindola@societao.com>
2018-12-15 00:17:56 -0600

    Se actualiza vista para inicio de sesión y actualización de los datos de perfil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,26 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :first_name, :last_name, :username, :email, :password, :password_confirmation, :rol_id])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :username, :email, :password, :password_confirmation, :rol_id])
   end
+
+  private 
+
+  def layout_by_resource
+  	if devise_controller? && params[:action].eql?('new')
+  		"devise"
+  	else
+  		"application"
+  	end
+  end
+
 end
