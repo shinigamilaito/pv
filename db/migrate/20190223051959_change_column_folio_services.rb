2af69d720f4212d3d299a083c2351cd9cacb0914
Edgar Espindola <edgareduardoespindola@gmail.com>
2019-02-23 11:43:00 -0600

    Se obtienen todos los datos de los servicios para poder realizar la migración

--- db/migrate/20190223051959_change_column_folio_services.rb
+++ db/migrate/20190223051959_change_column_folio_services.rb
@@ -2,7 +2,7 @@ class ChangeColumnFolioServices < ActiveRecord::Migration[5.1]
   def change
     add_column :services, :number_folio, :integer
 
-    Service.select(:id).order(:created_at).each_with_index do |service, index|
+    Service.order(:created_at).each_with_index do |service, index|
       service.number_folio = index + 1
       service.save
     end
